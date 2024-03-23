a9b1d1edec6e86556d2ecef8f5eb465f553a5826
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2024-03-23 20:54:00 -0300

    mq

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,27 @@ const pass = document.querySelector('#password')
 
 function gerarSenha(size, types) {
     let password = ''
+    const join = types.join('')
 
-    if (types.length == 0 || size < 4 || size > 128 || isNaN(size)) {
+    if (types.length == 0) {
         Toastify({
-            text: "Erro! Tente selecionar um tipo e/ou alterar o tamanho da senha (4 a 128).",
+            text: "Erro! Selecione um tipo de carácter.",
             duration: 3000,
-            gravity: "top", // `top` or `bottom`
-            position: "right", // `left`, `center` or `right`
+            gravity: "top",
+            position: "right",
             style: {
-              background: "#EB1400",
+                background: "#EB1400",
+            },
+        }).showToast();
+        return ''
+    } else if (size < 4 || size > 128 || isNaN(size)) {
+        Toastify({
+            text: "Erro! Insira um tamanho para a senha entre 4 e 128.",
+            duration: 3000,
+            gravity: "top",
+            position: "right",
+            style: {
+                background: "#EB1400",
             },
         }).showToast();
         return ''
@@ -25,8 +37,9 @@ function gerarSenha(size, types) {
 
     if (password.length < size) {
         while (password.length < size) {
-            const type = types[Math.floor(Math.random() * types.length)]
-            password += type[Math.floor(Math.random() * type.length)]
+            // const type = types[Math.floor(Math.random() * types.length)]
+            // password += type[Math.floor(Math.random() * type.length)]
+            password += join[Math.floor(Math.random() * join.length)]
         }
     }
 
